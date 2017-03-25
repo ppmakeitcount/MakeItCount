c70db9341c5704bda4ba9f8d858e3d773364142c
pp_makeitcount <pramod.potdar1@gmail.com>
2017-03-26 00:03:48 +0530

    Done
    App complete

--- index.ios.js
+++ index.ios.js
@@ -19,15 +19,27 @@ export default class MakeItCount extends Component {
     let tCount = this.state.count;
     return (
       <View style={styles.container}>
+
         <View style={styles.header}>
           <Text style={{fontSize:20, color:'#fff'}}>Make It Count</Text>
+
         </View>
-        <View style={styles.body}>
-          <TouchableOpacity onPress={this._onPressButton}>
-            <Text style={{fontSize:100, color:'#FF4081'}}>{tCount}</Text>
+
+        <View style={styles.count}>
+          <TouchableOpacity onPress={()=>this.setState({ count: this.state.count+1 })}>
+            <Text style={{fontSize:140, color:'#FF4081'}}>{tCount}</Text>
           </TouchableOpacity>
         </View>
-      </View>
+
+        <View style={styles.buttons}>
+          <TouchableOpacity onPress={()=>this.setState({ count: 0 })}>
+            <Text style={{fontSize:30, color:'#1A237E'}}>Clear</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=>this.setState({ count: this.state.count-1 })}>
+            <Text style={{fontSize:30, color:'#1A237E'}}>Minus</Text>
+          </TouchableOpacity>
+        </View>
+    </View>
     );
   }
 }
@@ -45,9 +57,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#3f51b5'
     //margin: 10,
   },
-  body: {
+  count: {
     flex:8,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e8eaf6'
+  },
+  buttons: {
+    flex:3,
+    flexDirection:'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#e8eaf6'
   },
